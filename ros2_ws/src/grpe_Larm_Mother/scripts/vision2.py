d7f06b4486de4da4503194f1739636872fb41f3b
Niranjanfr <niranjan.kulkarni@etu.imt-nord-europe.fr>
2024-01-12 11:39:26 +0100

    modification vision2

--- ros2_ws/src/grpe_Larm_Mother/scripts/vision2.py
+++ ros2_ws/src/grpe_Larm_Mother/scripts/vision2.py
@@ -34,7 +34,7 @@ color_info=(0, 0, 255)
 cap=cv2.VideoCapture(0)
 cv2.namedWindow('Camera')
 cv2.setMouseCallback('Camera', souris)
-hsv_px = [0,0,0]
+hsv_px = [47,142,120]
 
 # Creating morphological kernel
 kernel = np.ones((3, 3), np.uint8)
@@ -54,28 +54,29 @@ while True:
     cv2.putText(frame, "px HSV: "+pixel_hsv, (10, 260),
                font, 1, (255, 255, 255), 1, cv2.LINE_AA)
 
+
+
+    if cv2.waitKey(1)&0xFF==ord('q'):
+        break
+    # Flouttage de l'image
+    # image=cv2.blur(image, (7, 7))
+    # Erosion d'un mask
+    # mask=cv2.erode(mask, None, iterations=4)
+    # dilatation d'un mask
+    # mask=cv2.dilate(mask, None, iterations=4)
+
+    elements=cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+    if len(elements) > 0:
+        c=max(elements, key=cv2.contourArea)
+        ((x, y), rayon)=cv2.minEnclosingCircle(c)
+        if rayon>30:
+            cv2.circle(image2, (int(x), int(y)), int(rayon), color_info, 2)
+            cv2.circle(frame, (int(x), int(y)), 5, color_info, 10)
+            cv2.line(frame, (int(x), int(y)), (int(x)+150, int(y)), color_info, 2)
+            cv2.putText(frame, "Objet !!!", (int(x)+10, int(y) -10), cv2.FONT_HERSHEY_DUPLEX, 1, color_info, 1, cv2.LINE_AA)
     cv2.imshow('Camera', frame)
     cv2.imshow('image2', image2)
     cv2.imshow('Mask', mask)
 
-    if cv2.waitKey(1)&0xFF==ord('q'):
-        break
-# Flouttage de l'image
-image=cv2.blur(image, (7, 7))
-# Erosion d'un mask
-mask=cv2.erode(mask, None, iterations=4)
-# dilatation d'un mask
-mask=cv2.dilate(mask, None, iterations=4)
-
-elements=cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
-if len(elements) > 0:
-    c=max(elements, key=cv2.contourArea)
-    ((x, y), rayon)=cv2.minEnclosingCircle(c)
-    if rayon>30:
-        cv2.circle(image2, (int(x), int(y)), int(rayon), color_info, 2)
-        cv2.circle(frame, (int(x), int(y)), 5, color_info, 10)
-        cv2.line(frame, (int(x), int(y)), (int(x)+150, int(y)), color_info, 2)
-        cv2.putText(frame, "Objet !!!", (int(x)+10, int(y) -10), cv2.FONT_HERSHEY_DUPLEX, 1, color_info, 1, cv2.LINE_AA)
-        
 cap.release()
 cv2.destroyAllWindows()
