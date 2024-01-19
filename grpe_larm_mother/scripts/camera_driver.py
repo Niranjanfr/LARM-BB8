cfb232516c3380beeebb36cbcd15616cb535ebd9
Niranjanfr <niranjan.kulkarni@etu.imt-nord-europe.fr>
2024-01-19 16:27:54 +0100

    detection de 2 objets en meme temps

--- grpe_larm_mother/scripts/camera_driver.py
+++ grpe_larm_mother/scripts/camera_driver.py
@@ -169,8 +169,8 @@ class Realsense(Node):
         if len(elements) > 0:
             c=max(elements, key=cv2.contourArea)
             ((x, y), rayon)=cv2.minEnclosingCircle(c)
-            elements.pop(c)
-            c1 = max (elements, key=cv2.contourArea)
+            new_elements = tuple(x for x in elements if x != c)
+            c1 = max (new_elements, key=cv2.contourArea)
             ((x1, y1), rayon1)=cv2.minEnclosingCircle(c1)
             if (rayon<60 and rayon>40) and (rayon1 <60 and rayon1 > 40):
 
